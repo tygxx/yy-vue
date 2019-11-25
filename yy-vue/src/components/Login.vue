9251865453bb8c6024642ba0ef3a5522419f4f92
tygxx <251380405@qq.com>
2019-11-25 12:47:57 +0800

    Initial commit

---
+++ yy-vue/src/components/Login.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    用户名:<input type="text" v-model="loginForm.userName" placeholder="请输入用户名"/>
+    密码:<input type="password" v-model="loginForm.password" placeholder="请输入密码"/>
+  </div>
+</template>
+
+
